ff1128a5b2b027159135f5f4b7bb065fad8c458b
NiCHUY <romannichiporukk@gmail.com>
2024-01-27 12:19:38 +0300

    Added keyboard example Added strings example Added callback_query example

--- keyboards.py
+++ keyboards.py
@@ -5,5 +5,29 @@ def start_inline_keyboard():
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     markup.add(InlineKeyboardButton("Самостоятельные поездки", callback_data="self_travelling"),
-                InlineKeyboardButton("Организованные туры", callback_data="organized_travelling"))
+               InlineKeyboardButton("Организованные туры", callback_data="organized_travelling"))
     return markup
+
+
+def category_inline_keyboard():
+    category_markup = InlineKeyboardMarkup()
+    category_markup.row_width = 2
+    category_markup.add(InlineKeyboardButton("История", callback_data="history"),
+                        InlineKeyboardButton("Природа", callback_data="nature"),
+                        InlineKeyboardButton("Культура", callback_data="culture"),
+                        InlineKeyboardButton("С детьми", callback_data="with_kids"),
+                        InlineKeyboardButton("Спорт", callback_data="sport"),
+                        InlineKeyboardButton("Архитектура", callback_data="architecture"))
+    return category_markup
+
+
+def regions_inline_keyboard():
+    category_markup = InlineKeyboardMarkup()
+    category_markup.row_width = 2
+    category_markup.add(InlineKeyboardButton("Рядом с Минском", callback_data="near_minsk"),
+                        InlineKeyboardButton("Минская обл.", callback_data="minsk_region"),
+                        InlineKeyboardButton("Брестская обл.", callback_data="brest_region"),
+                        InlineKeyboardButton("Гомельская обл.", callback_data="gomel_region"),
+                        InlineKeyboardButton("Могилёвская обл.", callback_data="mogilev_region"),
+                        InlineKeyboardButton("Гродненская обл.", callback_data="grodno_region"))
+    return category_markup
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from keyboards import start_inline_keyboard
-from strings import hello_string_part_one, hello_string_part_two
+from keyboards import start_inline_keyboard, category_inline_keyboard
+from strings import hello_string_part_one, hello_string_part_two, category_string
 
 # Создание экземпляра бота с указанием токена вашего бота
 bot = telebot.TeleBot('6626087162:AAE5JpTAqgg6RykfG2YVdAZYQwRAxvqoKYM')
@@ -12,7 +12,10 @@ bot = telebot.TeleBot('6626087162:AAE5JpTAqgg6RykfG2YVdAZYQwRAxvqoKYM')
 @bot.message_handler(commands=['start'])    
 def handle_start(message):
     user_name = message.from_user.username
-    bot.send_message(message.chat.id, hello_string_part_one + '@' + user_name + hello_string_part_two, reply_markup=start_inline_keyboard())
+    bot.send_message(message.chat.id,
+                     hello_string_part_one + '@' + user_name + hello_string_part_two,
+                     parse_mode='Markdown',
+                     reply_markup=start_inline_keyboard())
 
 
 # Обработка команды /review
@@ -28,9 +31,17 @@ def handle_review(message):
 def callback_query(call):
     if call.data == "self_travelling":
         bot.answer_callback_query(call.id, "Переходим")
-        bot.send_message(call.message.chat.id, hello_string_part_one)
+        bot.send_message(call.message.chat.id,
+                         category_string,
+                         parse_mode='Markdown',
+                         reply_markup=category_inline_keyboard())
     elif call.data == "organized_travelling":
         bot.answer_callback_query(call.id, "Переходим")
+    elif call.data == "nature":
+        bot.answer_callback_query(call.id, "Природа")
+        bot.send_message(call.message.chat.id,
+                         category_string,
+                         parse_mode='Markdown')
 
 # @bot.message_handler(func=lambda message: True)
 # def message_handler(message):
